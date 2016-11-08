3024ade88a012f90c2dc9614bd5f47f1dd3cfe2d
Chris Behrens <chris@tilt.com>
2016-11-08 19:44:21 +0000

    Fix circle

--- circle/setup-go.sh
+++ circle/setup-go.sh
@@ -1,14 +1,20 @@
-#!/bin/sh -ex
+#!/bin/bash -ex
 
 cache_dir=${CIRCLE_CACHE_DIR:-.}
 
 go_pkg_loc='https://storage.googleapis.com/golang'
 go_pkg='go1.7.3.linux-amd64.tar.gz'
 
+go_pkg_cache="$cache_dir/$go_pkg"
+
 sudo rm -rf /usr/local/go
 
-if [ ! -e "$cache_dir/$go_pkg" ] ; then
-    curl -o "$cache_dir/$go_pkg" "$go_pkg_loc/$go_pkg"
+if [ ! -d "$cache_dir" ] ; then
+    mkdir "$cache_dir"
 fi
 
-sudo tar -C /usr/local -xzf "$cache_dir/$go_pkg"
+if [ ! -e "$go_pkg_cache" ] ; then
+    curl -o "$go_pkg_cache" "$go_pkg_loc/$go_pkg"
+fi
+
+sudo tar -C /usr/local -xzf "$go_pkg_cache"
